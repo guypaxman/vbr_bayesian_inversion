197e63b8a1b92cb355e300c32c24f08e939a043c
chrishavlin <chris.havlin@gmail.com>
2022-02-23 16:14:27 -0600

    test for bayes

--- tests/test_bayesian_inversion.m
+++ tests/test_bayesian_inversion.m
@@ -61,9 +61,12 @@ function [current_vals, diff_tolerances] = test_bayesian_inversion()
     % structure. Each row refers to the nested order of access for the
     % current_vals structure
     current_vals.arrays_to_compare= { ...
-                                      {'output'; 'Vm'} ...
+                                      {'results'; 'Vpo'} ...
+                                      {'results'; 'Xk1_temp'} ...
+                                      {'results'; 'Xk1_phi'} ...
+                                      {'results'; 'Xk1_g'} ...
                                     };
-    current_vals.output.results = results;  % ({'output'; 'Vm'} will be used to access output.Vm
+    current_vals.results = results;
 
 end
 
@@ -77,16 +80,15 @@ function [results_to_save, end_time] = call_bayesian_inversion()
 
 
     % load in the observations, limited by spatial extent
-    spatial_sampling.elim = -10.;     % eastern limit [decimal degrees; multiple of 0.5]
-    spatial_sampling.wlim = -80.;     % western limit [decimal degrees; multiple of 0.5]
-    spatial_sampling.nlim = 86.;      % nothern limit [decimal degrees; multiple of 0.5]
-    spatial_sampling.slim = 56.;      % southern limit [decimal degrees; multiple of 0.5]
-    spatial_sampling.xy_res = 2;  % spatial resolution [decimal degrees; multiple of 0.5]
-    spatial_sampling.z_start_index = 4; % index of the top depth [between 1 and 40].
-    spatial_sampling.z_end_index = 40;  % index of the bottom depth [between 1 and 40].
-    spatial_sampling.z_res = 1;         % depth sampling factor [each depth slice is 10 km].
+    spatial_sampling.elim = -10;
+    spatial_sampling.wlim = -80.;
+    spatial_sampling.nlim = 86.;
+    spatial_sampling.slim = 56.;
+    spatial_sampling.xy_res = 8;  % spatial resolution [decimal degrees; multiple of 0.5]
+    spatial_sampling.z_start_index = 4; % remove top 3 slices (10, 20, 30 km)
+    spatial_sampling.z_end_index = 40;
+    spatial_sampling.z_res = 10; % depth sampling factor
     observations = load_data(spatial_sampling);
-
     % load in the vbr predictions for selected method
     qmethod = 'xfit_premelt';     % Choose from xfit_premelt, xfit_mxw, eburgers_psp, andrade_psp
     vbr_predictions = load_vbr_box('sweep_box.mat', qmethod, observations);
